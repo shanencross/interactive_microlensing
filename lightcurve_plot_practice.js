bcd296203973c856a59f2a9e7a90f0e90e731b6c
Shanen Cross <shanencross@yahoo.com>
2016-09-19 12:37:02 -0700

    added ability to take any pair of arrays as input for plotting, separated calculating magnifications via equation, and  updated naming scheme

--- lightcurve_plot_practice.js
+++ lightcurve_plot_practice.js
@@ -10,31 +10,34 @@ D_rel = 1/(1/D_l - 1/D_s)
 pi_rel = 1/D_rel
 theta_E = sqrt(k*mass*pi_rel)
 
-t_E = theta_E / mu_rel
+tE = theta_E / mu_rel
 
-time_term = (t - t_max)/t_E
-u = sqrt(u_0**2 + time_term**2)
+timeTerm = (t - tMax)/tE
+u = sqrt(u0**2 + timeTerm**2)
 magnif(u) = (u**2 + 2)/(u * sqrt(u**2 + 4))
 
 
-OK let's say t_E=10 days, t_max = 15 days, u_0 = 0.1
+OK let's say tE=10 days, tMax = 15 days, u0 = 0.1
 
-so we get time_term from t_max and t_E and t
-then we get u from time_term and u_0
+so we get timeTerm from tMax and tE and t
+then we get u from timeTerm and u0
 then we get magnif from u
 
 Also, there are 30 days per x_pixel (0 to 30)
 and 4 magnification units per y_pixel (0 to 4)
 */
 
-var t_E = 10;
-var t_max = 15;
-var u_0 = 0.1;
+var tE = 10;
+var tMax = 15;
+var u0 = 0.1;
 
-var day_width = 30;
-var magnif_height = 10;
-var xPixelScale = canvas.width/day_width; // pixels per day
-var yPixelScale = canvas.height/magnif_height; // pixels per magnif unit
+var dayWidth = 50;
+var magnifHeight = 10;
+var xPixelScale = canvas.width/dayWidth; // pixels per day
+var xPlotStep = 1; // Spacing between vertical lines in days
+var yPixelScale = canvas.height/magnifHeight; // pixels per magnif unit
+var yPlotStep = 1; // Spacing between horizontal lines in magnification units
+
 var initialT = 0;
 var dt = 0.1;
 
@@ -47,123 +50,169 @@ var tMaxReadout = document.getElementById("tMaxReadout");
 var u0slider = document.getElementById("u0slider");
 var u0readout = document.getElementById("u0readout");
 
-window.onload = initialize();
+window.onload = init;
 
-function initialize() {
-  initializeListeners();
-  initializePlot();
+function init() {
+  initListeners();
+  initPlot();
   plotLightcurve();
 }
 
-function initializeListeners() {
-  tEslider.addEventListener("input", update_tE, false);
-  tEslider.addEventListener("change", update_tE, false);
+function initListeners() {
+  tEslider.addEventListener("input", updateTE, false);
+  tEslider.addEventListener("change", updateTE, false);
 
-  tMaxSlider.addEventListener("input", update_tMax, false);
-  tMaxSlider.addEventListener("change", update_tMax, false);
+  tMaxSlider.addEventListener("input", updateTMax, false);
+  tMaxSlider.addEventListener("change", updateTMax, false);
 
-  u0slider.addEventListener("input", update_u0, false);
-  u0slider.addEventListener("change", update_u0, false);
+  u0slider.addEventListener("input", updateU0, false);
+  u0slider.addEventListener("change", updateU0, false);
 }
 
-function update_tE() {
+function updateTE() {
   tEreadout.innerHTML = tEslider.value;
-  t_E = tEslider.value;
+  tE = tEslider.value;
   context.clearRect(0, 0, canvas.width, canvas.height);
   plotLightcurve();
 }
 
-function update_tMax() {
+function updateTMax() {
   tMaxReadout.innerHTML = tMaxSlider.value;
-  t_max = tMaxSlider.value;
+  tMax = tMaxSlider.value;
   context.clearRect(0, 0, canvas.width, canvas.height);
   plotLightcurve();
 }
 
-function update_u0() {
+function updateU0() {
   u0readout.innerHTML = u0slider.value;
-  u_0 = u0slider.value;
+  u0 = u0slider.value;
   context.clearRect(0, 0, canvas.width, canvas.height);
   plotLightcurve();
 }
 
 
-function initializePlot() {
+function initPlot() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   // fill in background
   context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.lineWidth = 1;
+  context.strokeStyle = "black";
+  context.lineWidth = 0.1;
   // draw vertical lines
-  for (var xPlotDay = 0; xPlotDay < day_width; xPlotDay++) {
+  for (var xPlotDay = 0; xPlotDay < dayWidth; xPlotDay+=xPlotStep) {
     var xPlotPixel = xPlotDay * xPixelScale;
     context.beginPath();
     context.moveTo(xPlotPixel, 0)
     context.lineTo(xPlotPixel, canvas.height);
-    context.strokeStyle = "black";
     context.stroke();
   }
 
   //draw horizontal lines;
-  for (var yPlotMagnif = 0; yPlotMagnif < magnif_height; yPlotMagnif++) {
+  for (var yPlotMagnif = 0; yPlotMagnif < magnifHeight; yPlotMagnif+=yPlotStep) {
     var yPlotPixel = yPlotMagnif * yPixelScale;
     context.beginPath();
     context.moveTo(0, yPlotPixel);
     context.lineTo(canvas.width, yPlotPixel);
-    context.strokeStyle = "black";
     context.stroke();
   }
 }
 
-function plotLightcurve() {
-  initializePlot();
-  var prevTday = initialT;
-  var prevMagnif = get_magnif(initialT);
+function plotLightcurve(inputData, fromEquation=false) {
+  initPlot();
+  context.lineWidth = 2;
+  context.strokeStyle = "blue";
+  if (fromEquation)
+    plotLightcurveFromEquation();
+  else
+    plotLightcurveFromData(inputData);
+}
 
-  for (var tDay=initialT + dt; tDay <= day_width; tDay += dt) {
-    var magnif = get_magnif(tDay);
+function plotLightcurveFromData(inputData) {
+  var curveData;
+  if (inputData === undefined) {
+    curveData = getCurveData();
+  }
+  else {
+    curveData = inputData;
+  }
 
-    var tPixel = tDay * xPixelScale;
-    var prevTpixel = prevTday * xPixelScale;
+  var times = curveData.times;
+  var magnifs = curveData.magnifs;
 
-    var magnifPixel = canvas.height - magnif * yPixelScale;
-    var prevMagnifPixel = canvas.height - prevMagnif * yPixelScale;
+  var prevTday = times[0];
+  var prevMagnif = magnifs[0];
 
-    context.beginPath();
-    context.moveTo(prevTpixel, prevMagnifPixel);
-    context.lineTo(tPixel, magnifPixel);
-    context.lineWidth = 2;
-    context.strokeStyle = "blue";
-    context.stroke();
+  for (var i=0; i<times.length; i++) {
+    var tDay = times[i];
+    var magnif = magnifs[i];
 
+    plotLine(prevTday, prevMagnif, tDay, magnif);
     prevTday = tDay;
     prevMagnif = magnif;
   }
 }
-magnif_max = get_magnif(t_max);
-//console.log(magnif_max);
 
-function get_time_term(t) {
-  time_term = (t - t_max)/t_E;
-  return time_term;
+function plotLightcurveFromEquation() {
+  var prevTday = initialT;
+  var prevMagnif = getMagnif(initialT);
+
+  for (var tDay=initialT + dt; tDay <= dayWidth; tDay += dt) {
+    var magnif = getMagnif(tDay);
+
+    plotLine(prevTday, prevMagnif, tDay, magnif);
+    prevTday = tDay;
+    prevMagnif = magnif;
+  }
 }
 
-function get_u(time_term) {
-  u = Math.sqrt(u_0*u_0 + time_term*time_term)
+function plotLine(prevTday, prevMagnif, tDay, magnif) {
+  var tPixel = tDay * xPixelScale;
+  var prevTpixel = prevTday * xPixelScale;
+
+  var magnifPixel = canvas.height - magnif * yPixelScale;
+  var prevMagnifPixel = canvas.height - prevMagnif * yPixelScale;
+
+  context.beginPath();
+  context.moveTo(prevTpixel, prevMagnifPixel);
+  context.lineTo(tPixel, magnifPixel);
+  context.stroke();
+}
+
+function getCurveData() {
+  var times = [];
+  var magnifs = [];
+
+  for (var tDay=0; tDay <= dayWidth; tDay += dt) {
+    var magnif = getMagnif(tDay);
+    times.push(tDay);
+    magnifs.push(magnif);
+  }
+
+  var curveData = {times:times, magnifs:magnifs};
+  return curveData;
+}
+
+function getTimeTerm(t) {
+  timeTerm = (t - tMax)/tE;
+  return timeTerm;
+}
+
+function getU(timeTerm) {
+  u = Math.sqrt(u0*u0 + timeTerm*timeTerm)
   return u;
 }
 
-function get_magnif_from_u(u) {
-  var magnif_numerator = u*u + 2;
-  var magnif_denominator = u * Math.sqrt(u * u + 4);
-  magnif = magnif_numerator / magnif_denominator;
+function getMagnifFromU(u) {
+  var magnifNumerator = u*u + 2;
+  var magnifDenominator = u * Math.sqrt(u * u + 4);
+  magnif = magnifNumerator / magnifDenominator;
   return magnif;
 }
 
-function get_magnif(t) {
-  var time_term = get_time_term(t);
-  var u = get_u(time_term);
-  var magnif = get_magnif_from_u(u);
+function getMagnif(t) {
+  var timeTerm = getTimeTerm(t);
+  var u = getU(timeTerm);
+  var magnif = getMagnifFromU(u);
   return magnif;
 }
