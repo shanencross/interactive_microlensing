38c2b49041408ab7b9bea5a68010ea3bf5b8fa3d
shanencross <shanencross@yahoo.com>
2017-06-26 17:59:56 -0700

    removed backups and added django page

--- src/main.js
+++ src/main.js
@@ -12,12 +12,22 @@ var animationModule = require("./fspl-microlensing-event-animation.js");
 /** init */
 function init() {
   // add event listener to show/hide link for finite source options
-  showOrHideModule.init();
+  if (typeof showOrHideModule !== "undefined" &&
+      showOrHideModule !== null)
+    showOrHideModule.init();
 
-  // these modules must be initialized in specific order
-  eventModule.init();
-  lensPlaneModule.init();
-  animationModule.init();
+  // these modules must be initialized in this specific order
+  if (typeof eventModule !== "undefined" &&
+      eventModule !== null)
+    eventModule.init();
+
+  if (typeof lensPlaneModule !== "undefined" &&
+      lensPlaneModule !== null)
+    lensPlaneModule.init();
+
+  if (typeof animationModule !== "undefined" &&
+      animationModule !== null)
+    animationModule.init();
 }
 
 window.onload = init;
